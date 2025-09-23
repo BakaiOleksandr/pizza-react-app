168dd5a00427804ca97b503b6afaeb60305da221
Oleksandr Bakai <saniabakay@gmail.com>
2025-09-23 21:36:08 +0200

    1

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,26 +1,32 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 import PizzaCard from '../components/PizzaCard';
 import './Menu.css';
 
 export default function Menu({pizzas}) {
   //get list of all igredients
-  const newArrayOfIngredients = [].concat(
-    ...pizzas.map((el) => el.ingredients)
-  );
-  const countItems = {};
-  for (const item of newArrayOfIngredients) {
-    countItems[item] = countItems[item] ? countItems[item] + 1 : 1;
-  }
-  const listOfIngredients = Object.keys(countItems).filter(
-    (item) => countItems[item] > 1
-  );
-  console.log(`NEW ARRAY ${newArrayOfIngredients}`);
+  const listOfIngredients = useMemo(() => {
+    const newArrayOfIngredients = [].concat(
+      ...pizzas.map((el) => el.ingredients)
+    );
+    console.log(`NEW ARRAY ${newArrayOfIngredients}`);
+    const countItems = {};
+    for (const item of newArrayOfIngredients) {
+      countItems[item] = countItems[item] ? countItems[item] + 1 : 1;
+    }
+
+    return Object.keys(countItems).filter((item) => countItems[item] > 1);
+  }, [pizzas]);
+  ////
+
   console.log(`INGREDIENTS ${listOfIngredients}`);
   //////
 
   //State
   const [pizzasState, setPizzasState] = useState([]);
-  const [priceState, setPriceState] = useState(true);
+  const [priceState, setPriceState] = useState(false);
+  const [ingredientOfPizza, setIngredientOfPizza] = useState([]);
+  const [priceActive, setPriceActive] = useState(false);
+
   ///
 
   // sorting Price High Low
@@ -34,9 +40,11 @@ export default function Menu({pizzas}) {
   }
   ///
 
+  //Price toggle
   const PriceOnToggle = () => {
     priceState ? PriceLow() : PriceHigh();
     setPriceState(!priceState);
+    setPriceActive(true);
   };
 
   //Changing pizzas State on event
@@ -49,15 +57,32 @@ export default function Menu({pizzas}) {
     <PizzaCard key={pizza.id} pizza={pizza} />
   ));
 
-  //FilterIngredient
-  const FilterIngredient = (ingredient) => {
-    const filterIngredient = pizzas.filter((pizza) => {
-      return pizza.ingredients.includes(ingredient);
+  //ToggleIngredient
+  const ToggleIngredient = (ingredient) => {
+    setIngredientOfPizza((prev) => {
+      return prev.includes(ingredient)
+        ? prev.filter((i) => i !== ingredient)
+        : [...prev, ingredient];
     });
-    setPizzasState(filterIngredient);
   };
+  //Filtering pizzas by ingredients
+  useEffect(() => {
+    let filtered = pizzas;
 
-  ////
+    if (ingredientOfPizza.length > 0) {
+      filtered = pizzas.filter((pizza) =>
+        ingredientOfPizza.every((ing) => pizza.ingredients.includes(ing))
+      );
+    }
+
+    if (priceActive) {
+      filtered = [...filtered].sort((a, b) =>
+        priceState ? a.price - b.price : b.price - a.price
+      );
+    }
+
+    setPizzasState(filtered);
+  }, [ingredientOfPizza, pizzas, priceState, priceActive]);
 
   //////////////////////
   return (
@@ -69,7 +94,15 @@ export default function Menu({pizzas}) {
       </button>
 
       {/* REFRESH */}
-      <button className="btn refresh" onClick={() => setPizzasState(pizzas)}>
+      <button
+        className="btn refresh"
+        onClick={() => {
+          setPizzasState([...pizzas]);
+          setIngredientOfPizza([]);
+          setPriceActive(false);
+          setPriceState(false);
+        }}
+      >
         Refresh
       </button>
       {/* ///// */}
@@ -77,18 +110,24 @@ export default function Menu({pizzas}) {
       <br></br>
 
       <div className="ingredients">Ingredients</div>
-      {listOfIngredients.map((ingredient, index) => (
+      {listOfIngredients.map((ingredient) => (
         <button
-          className="btn"
-          key={index}
-          onClick={() => FilterIngredient(ingredient)}
+          className={`btn ${
+            ingredientOfPizza.includes(ingredient) ? 'active' : ''
+          }`}
+          key={ingredient}
+          onClick={() => ToggleIngredient(ingredient)}
         >
           {ingredient}
         </button>
       ))}
-
+      <div className="found">Pizzas found: {pizzasState.length}</div>
       <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {mapped}
+        {pizzasState.length > 0 ? (
+          mapped
+        ) : (
+          <div className="found">Not found</div>
+        )}
       </div>
     </>
   );
