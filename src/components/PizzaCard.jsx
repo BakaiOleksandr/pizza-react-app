5a3d978affc4c77dbdff6383334c32bc50d3de8d
Oleksandr Bakai <saniabakay@gmail.com>
2025-09-23 18:40:49 +0200

    commit

--- src/components/PizzaCard.jsx
+++ src/components/PizzaCard.jsx
@@ -1,30 +1,17 @@
-import './PizzaCard.css'
+import './PizzaCard.css';
 
-export default function PizzaCard({ pizza }) {
+export default function PizzaCard({pizza}) {
   return (
-
     <div className="pizza-container">
-
-
-
-      <div className='pizza-img-container'>
-      <img 
-        src={pizza.image}
-        alt={pizza.name}
-        className="pizza-img"
-      />
+      <div className="pizza-img-container">
+        <img src={pizza.image} alt={pizza.name} className="pizza-img" />
       </div>
-      <div className='pizza-items-container'>
-      <h2 className="izza-h2">{pizza.name}</h2>
-      <p className="pizza-ingredients">{pizza.ingredients.join(', ')}</p>
-      <p className="pizza-price">{pizza.price} $</p>
-      <button className="pizza-button">
-        Add to Cart
-      </button>
+      <div className="pizza-items-container">
+        <h2 className="izza-h2">{pizza.name}</h2>
+        <p className="pizza-ingredients">{pizza.ingredients.join(', ')}</p>
+        <p className="pizza-price">{pizza.price} $</p>
+        <button className="pizza-button">Add to Cart</button>
       </div>
-
-
-
     </div>
-  )
+  );
 }
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -14,8 +14,8 @@ export default function Menu({pizzas}) {
   const listOfIngredients = Object.keys(countItems).filter(
     (item) => countItems[item] > 1
   );
-  console.log(newArrayOfIngredients);
-  console.log(listOfIngredients);
+  console.log(`NEW ARRAY ${newArrayOfIngredients}`);
+  console.log(`INGREDIENTS ${listOfIngredients}`);
   //////
 
   //State
@@ -49,25 +49,44 @@ export default function Menu({pizzas}) {
     <PizzaCard key={pizza.id} pizza={pizza} />
   ));
 
+  //FilterIngredient
+  const FilterIngredient = (ingredient) => {
+    const filterIngredient = pizzas.filter((pizza) => {
+      return pizza.ingredients.includes(ingredient);
+    });
+    setPizzasState(filterIngredient);
+  };
+
+  ////
+
   //////////////////////
   return (
     <>
       <div className="filters">Filters</div>
+
       <button className="btn price" onClick={PriceOnToggle}>
         Price {priceState ? 'Low→High' : 'High→Low'}
       </button>
+
       {/* REFRESH */}
       <button className="btn refresh" onClick={() => setPizzasState(pizzas)}>
         Refresh
       </button>
       {/* ///// */}
+
       <br></br>
+
       <div className="ingredients">Ingredients</div>
       {listOfIngredients.map((ingredient, index) => (
-        <button className="btn" key={index}>
+        <button
+          className="btn"
+          key={index}
+          onClick={() => FilterIngredient(ingredient)}
+        >
           {ingredient}
         </button>
       ))}
+
       <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {mapped}
       </div>
